cf651cb8f60c0b62006ef028960e71b833372924
HassanAliMAli <hassanali205031@gmail.com>
2026-05-21 14:40:15 +0500

    feat: standardize neovim on catppuccin mocha theme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      integrations = {
+        telescope = true,
+        lazy = true,
+        treesitter = true,
+        notify = true,
+        mini = true,
+        lsp_trouble = true,
+        which_key = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
